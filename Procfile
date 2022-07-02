c938dc822d4e245309e65dbe18886dc75554e64c
Tristan1649 <c.bapanov@gmail.com>
2022-07-02 13:40:30 +0600

    first commit

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-wed: gunicorn restaraunt.wsgi
+web: gunicorn restaraunt.wsgi
--- git.sh
+++ git.sh
@@ -3,11 +3,12 @@
 
 echo "Start"
 
-git init &&
+
+git init
+git add --all
+git commit -a -m 'first commit'
 git remote add origin git@github.com:Tristan1649/Restaraunt.git
-git checkout -b chingiz
-git add . &&
-git commit -m "all" &&
-git push origin chingiz
+git pull --rebase origin master
+git push origin master
 
 echo "Finish"
